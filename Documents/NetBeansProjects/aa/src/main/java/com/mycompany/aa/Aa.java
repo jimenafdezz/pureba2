548573217560e666fe2828b8b2584ff55870c385
jimenafdezz <153528628+jimenafdezz@users.noreply.github.com>
2024-06-07 16:13:57 +0200

    Aa.java1

--- Documents/NetBeansProjects/aa/src/main/java/com/mycompany/aa/Aa.java
+++ Documents/NetBeansProjects/aa/src/main/java/com/mycompany/aa/Aa.java
@@ -8,6 +8,6 @@ package com.mycompany.aa;
 public class Aa {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("ola cucu");
     }
 }
