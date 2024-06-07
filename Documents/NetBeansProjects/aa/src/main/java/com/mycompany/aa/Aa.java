de7ece9f86d249e43f769a993a90cd3206e9f856
jimenafdezz <jimena.fernandezlabrador@iesmiguelherrero.com>
2024-06-07 15:46:33 +0200

    aa hola mundo

---
+++ Documents/NetBeansProjects/aa/src/main/java/com/mycompany/aa/Aa.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.aa;
+
+/**
+ *
+ * @author daw107
+ */
+public class Aa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
