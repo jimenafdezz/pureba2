da9ab5c9d6a0220ffbb539fd1228290663bf59d0
jimenafdezz <153528628+jimenafdezz@users.noreply.github.com>
2024-06-07 16:25:47 +0200

    Update Aa.java

--- Documents/NetBeansProjects/aa/src/main/java/com/mycompany/aa/Aa.java
+++ Documents/NetBeansProjects/aa/src/main/java/com/mycompany/aa/Aa.java
@@ -8,6 +8,6 @@ package com.mycompany.aa;
 public class Aa {
 
     public static void main(String[] args) {
-        System.out.println("ola klnhjmio");
+        System.out.println("olaaaaaaaaaa");
     }
 }
